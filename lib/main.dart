3dccb8559ed5e7ddddabfa722ba687819491fd43
therokibul <therokibul@gmail.com>
2021-10-06 15:19:36 +0600

    day13

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,75 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
-void main() {
+void main(){
   runApp(
-    MaterialApp(
+      MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Home Page'),
+          title: Text(
+            'Day 12'
+          ),
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'Lorem Ipsum is 1',
-                style: TextStyle(
-                  fontSize: 40,
+              Container(
+                color: Colors.black54,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(25),
+                width: 300,
+                child: Text(
+                  'Rokibul Hasan',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30.0
+                  ),
                 ),
               ),
-              Text(
-                'Lorem Ipsum is 2',
-                style: TextStyle(
-                  fontSize: 30,
+              Container(
+                color: Colors.black54,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(25),
+                width: 300,
+                child: Text(
+                  '@therokibul',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 30.0
+                  ),
                 ),
               ),
-              Text(
-                'Lorem Ipsum is 3',
-                style: TextStyle(
-                  fontSize: 20,
+              Container(
+                color: Colors.black54,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(25),
+                width: 300,
+                child: Text(
+                  'contact@rokibul.com',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20.0
+                  ),
                 ),
               ),
+
+
             ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
