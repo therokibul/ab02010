61e7b901bb606e26f32c60cd41018232d5fc0dbe
therokibul <therokibul@gmail.com>
2021-10-06 15:18:20 +0600

    day11 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,39 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: Text('PROFILE'),
+          title: Text('Home Page'),
         ),
-        body: HomePage(),
+        body: Center(
+          child: Column(
+            children: [
+              Text(
+                'Lorem Ipsum is 1',
+                style: TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+              Text(
+                'Lorem Ipsum is 2',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                'Lorem Ipsum is 3',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        CircleAvatar(
-          radius: 90,
-          backgroundImage: AssetImage('images/lash.jpg'),
-        ),
-        Container(
-          width: 300,
-          color: Colors.black,
-          margin: EdgeInsets.symmetric(vertical: 20),
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Your Name',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-        ),
-        Container(
-          width: 300,
-          color: Colors.black,
-          margin: EdgeInsets.symmetric(vertical: 20),
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Your Name',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-        ),
-        Container(
-          width: 300,
-          color: Colors.black,
-          margin: EdgeInsets.symmetric(vertical: 20),
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Your Name',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-        ),
-        Container(
-          width: 300,
-          color: Colors.black,
-          margin: EdgeInsets.symmetric(vertical: 20),
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Your Name',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-        ),
-        Container(
-          width: 300,
-          color: Colors.black,
-          margin: EdgeInsets.symmetric(vertical: 20),
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Your Name',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 40, color: Colors.white),
-          ),
-        ),
-      ],
-    );
-  }
+    ),
+  );
 }
