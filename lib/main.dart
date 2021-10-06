e5bd1d6f87b6f1ed0140532953635024f5467a8f
therokibul <therokibul@gmail.com>
2021-10-06 15:21:20 +0600

    day14 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(
-      MyApp(),
+    MyApp(),
   );
 }
 
@@ -13,62 +11,69 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Day 12'
-          ),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.black54,
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(25),
-                width: 300,
-                child: Text(
-                  'Rokibul Hasan',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30.0
-                  ),
-                ),
-              ),
-              Container(
-                color: Colors.black54,
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(25),
-                width: 300,
-                child: Text(
-                  '@therokibul',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30.0
-                  ),
-                ),
-              ),
-              Container(
-                color: Colors.black54,
-                padding: EdgeInsets.all(20),
-                margin: EdgeInsets.all(25),
-                width: 300,
-                child: Text(
-                  'contact@rokibul.com',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 20.0
-                  ),
-                ),
-              ),
+        backgroundColor: Colors.black26,
+        body: HomePage(),
+      ),
+    );
+  }
+}
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-            ],
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 25,
           ),
-        ),
+          Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1610384467831-4b1e0cd08336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+            height: 200,
+          ),
+          Container(
+            width: 250,
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(vertical: 20),
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            color: Colors.black,
+            child: Text(
+              'Rokibul Hasan',
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ),
+          Container(
+            width: 250,
+            alignment: Alignment.center,
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            color: Colors.black,
+            child: Text(
+              '@therokibul',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+          ),
+          Container(
+            width: 250,
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(vertical: 20),
+            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+            color: Colors.black,
+            child: Text(
+              'contact@rokibul.com',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+        ],
       ),
     );
   }
