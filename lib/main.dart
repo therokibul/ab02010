365b76627f35dce6903d58d3a0ee38232e4e672b
therokibul <therokibul@gmail.com>
2021-10-11 17:17:06 +0600

    day15 completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black26,
+        backgroundColor: Colors.teal,
         body: HomePage(),
       ),
     );
@@ -30,51 +30,108 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 25,
-          ),
-          Image(
-            image: NetworkImage(
-                'https://images.unsplash.com/photo-1610384467831-4b1e0cd08336?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
-            height: 200,
-          ),
-          Container(
-            width: 250,
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(vertical: 20),
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-            color: Colors.black,
-            child: Text(
-              'Rokibul Hasan',
-              style: TextStyle(color: Colors.white, fontSize: 30),
+    return ListView(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'Settings',
+              style: TextStyle(color: Colors.white),
             ),
-          ),
-          Container(
-            width: 250,
-            alignment: Alignment.center,
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-            color: Colors.black,
-            child: Text(
-              '@therokibul',
-              style: TextStyle(color: Colors.white, fontSize: 25),
+            Text(
+              'Profile',
+              style: TextStyle(fontSize: 30, color: Colors.white),
             ),
-          ),
-          Container(
-            width: 250,
-            alignment: Alignment.center,
-            margin: EdgeInsets.symmetric(vertical: 20),
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-            color: Colors.black,
-            child: Text(
-              'contact@rokibul.com',
-              style: TextStyle(color: Colors.white, fontSize: 20),
+            Text(
+              'Log out',
+              style: TextStyle(color: Colors.white),
             ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        CircleAvatar(
+          radius: 70,
+          backgroundImage: AssetImage('images/lash.jpg'),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          'Victoria Robertson',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 30),
+        ),
+        Text(
+          'A mantra goes here',
+          textAlign: TextAlign.center,
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+            size: 40,
           ),
-        ],
-      ),
+          title: Text('Header'),
+          subtitle: Text(
+              "He'll want to use your yacht, and I don't want this thing smelling like fish"),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+            size: 40,
+          ),
+          title: Text('Header'),
+          subtitle: Text(
+              "He'll want to use your yacht, and I don't want this thing smelling like fish"),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+            size: 40,
+          ),
+          title: Text('Header'),
+          subtitle: Text(
+              "He'll want to use your yacht, and I don't want this thing smelling like fish"),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+            size: 40,
+          ),
+          title: Text('Header'),
+          subtitle: Text(
+              "He'll want to use your yacht, and I don't want this thing smelling like fish"),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+            size: 40,
+          ),
+          title: Text('Header'),
+          subtitle: Text(
+              "He'll want to use your yacht, and I don't want this thing smelling like fish"),
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.flutter_dash_outlined,
+            color: Colors.white,
+            size: 40,
+          ),
+          title: Text('Header'),
+          subtitle: Text(
+              "He'll want to use your yacht, and I don't want this thing smelling like fish"),
+        ),
+      ],
     );
   }
 }
